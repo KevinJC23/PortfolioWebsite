d5a7c0ba9b241423495f072e6ab1bd573156c197
Kevin Juan Carlos <142558666+KevinJC23@users.noreply.github.com>
2025-07-12 10:59:00 +0700

    Update Code

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -56,7 +56,7 @@ const Hero = () => {
               transition={{delay: 1.0}}
               className="flex flex-row sm:flex-row justify-center items-center gap-3 sm:gap-4"
             >
-                <Link href="https://drive.google.com/file/d/1dEk-TPYabN0Gu3yGE5N_YOlrknO9qkEd/view?usp=drive_link" passHref>
+                <Link href="https://drive.google.com/file/d/1x0gcJKpoe0oNx2oW7Worpl6gOcJqd9_P/view?usp=drive_link" passHref>
                     <HoverBorderGradient
                         containerClassName="rounded-full w-fit"
                         as="button"
